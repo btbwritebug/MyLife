c14b54046958a86d29b91b9197c0cccfd2ea937c
Bao <baotingbo@chinasie.com>
2023-06-28 10:09:25 +0800

    hello world

--- MyLife/src/main/java/com/btb/controller/GitController.java
+++ MyLife/src/main/java/com/btb/controller/GitController.java
@@ -1,8 +1,16 @@
 package com.btb.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/gitHub")
+@Api(value = "Git推送接口", tags = {"Git推送接口"})
 public class GitController {
-
+    @GetMapping("/hello")
+    public String test() {
+        return "hello world";
+    }
 }
